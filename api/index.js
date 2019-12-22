9f2df290e9d21b478f94c11c9dc07ba9fa310468
cumet04 <medalhkr@gmail.com>
2019-12-22 09:57:08 +0000

    call twitter

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import Twitter from 'twitter'
 
 const app = express()
 
+// ----------
+
 app.get('/', function(req, res) {
-  res.send('hello world')
+  const client = Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+  client.get('statuses/home_timeline.json', { count: 10 }).then(resp => {
+    res.send(resp)
+  })
 })
+
+// ----------
 
 module.exports = {
   path: '/api',
