ff8fd7a42029a92992f818ebb814c39895809bf9
cumet04 <medalhkr@gmail.com>
2019-12-22 14:31:15 +0000

    minimal tweet component

---
+++ components/Tweet.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <article>
+      <p class="info">
+        <span class="username">{{ username }}</span>
+        <span class="userid">@{{ userid }}</span>
+        <span class="created_at">{{ created_at }}</span>
+      </p>
+      <p class="text">{{ text }}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tweet'],
+  computed: {
+    username() {
+      return this.tweet.user.name
+    },
+    userid() {
+      return this.tweet.user.screen_name
+    },
+    text() {
+      return this.tweet.text
+    },
+    created_at() {
+      return this.tweet.created_at
+    }
+  }
+}
+</script>
+
+<style>
+article {
+  border: solid 1px gray;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <ul>
+      <li v-for="tweet in tweets" :key="tweet.id">
+        <tweet :tweet="tweet"></tweet>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import Tweet from '~/components/Tweet.vue'
+
+export default {
+  components: {
+    tweet: Tweet
+  },
+  async asyncData({ $axios }) {
+    return {
+      tweets: await $axios.$get('/api/')
+    }
+  }
+}
 </script>
 
 <style></style>
