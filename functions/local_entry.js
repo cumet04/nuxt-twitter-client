b31aa8b41656cb2932b79ab590ae3482d44932d4
cumet04 <medalhkr@gmail.com>
2019-12-29 04:16:59 +0000

    add functions log

--- functions/local_entry.js
+++ functions/local_entry.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const logger = require("morgan");
 const app = require("./app.js");
 
 global.app_config = require("./.app_config.json");
 
 express()
+  .use(logger("short"))
   .use("/api", app.router)
   .listen(process.env.PORT || 5000, "0.0.0.0", function() {
     console.log(
