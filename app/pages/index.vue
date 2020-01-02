c5f6b9e78493ecd950456d3f9704a62560a60190
cumet04 <medalhkr@gmail.com>
2020-01-02 07:29:42 +0000

    extract fetching timeline to methods

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,22 +20,29 @@ export default {
   components: {
     tweet: Tweet
   },
-  async asyncData({ store }) {
+  props: ['tweets'],
+  mounted() {
     // FIXME: wait for tokens set
+    setTimeout(this.fetch_timeline, 1000)
+  },
+  methods: {
+    fetch_timeline() {
+      // firebase ignores emulator setting when functions() is called with region
+      const func = process.env.dev_functions_emulator
+        ? firebase.app().functions()
+        : firebase.app().functions('asia-northeast1')
 
-    // firebase ignores emulator setting when functions() is called with region
-    const func = process.env.dev_functions_emulator
-      ? firebase.app().functions()
-      : firebase.app().functions('asia-northeast1')
-
-    const resp = await func.httpsCallable('home')({
-      twitter: {
-        access_token_key: store.state.auth.token,
-        access_token_secret: store.state.auth.secret
-      }
-    })
-    return {
-      tweets: resp.data
+      // TODO: [Vue warn]: Avoid mutating a prop directly since the value will be overwritten whenever the parent component re-renders. Instead, use a data or computed property based on the prop's value. Prop being mutated: "tweets"
+      func
+        .httpsCallable('home')({
+          twitter: {
+            access_token_key: this.$store.state.auth.token,
+            access_token_secret: this.$store.state.auth.secret
+          }
+        })
+        .then(resp => {
+          this.tweets = resp.data
+        })
     }
   }
 }
